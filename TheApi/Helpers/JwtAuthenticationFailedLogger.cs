a5058bccf2cdf49a222ed46852eb46591f1ec376
Wesley Cabus <wesley@gotsharp.be>
2025-03-07 21:25:58 +0100

    This code prevented running any of the tests.
    It started the response way too soon, causing the test to always fail

--- TheApi/Helpers/JwtAuthenticationFailedLogger.cs
+++ TheApi/Helpers/JwtAuthenticationFailedLogger.cs
@@ -2,25 +2,26 @@ namespace TheApi.Helpers;
 
 public static class JwtAuthenticationFailedLogger
 {
-    public static async Task WriteAuthenticationFailed(AuthenticationFailedContext context)
+    public static Task WriteAuthenticationFailed(AuthenticationFailedContext context)
     {
-        ILogger logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
-
-        logger.LogError($"Authentication failed for {context.Principal?.Identity?.Name} to {context.Request.Path}: {context.Exception.Message}");
-        ApiResponse res = new ApiResponse
-        {
-            Success = false,
-            Message = "401: Authentication failed"
-        };
-        try
-        {
-            context.Response.StatusCode = 401;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, $"Failed to set status code to 401 when athentication failed for {context.Principal?.Identity?.Name} to {context.Request.Path}: {ex.Message}");
-            // Don't fail trying to error
-        }
-        await context.Response.WriteAsJsonAsync(res);
+        return Task.CompletedTask;
+        // ILogger logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+        //
+        // logger.LogError($"Authentication failed for {context.Principal?.Identity?.Name} to {context.Request.Path}: {context.Exception.Message}");
+        // ApiResponse res = new ApiResponse
+        // {
+        //     Success = false,
+        //     Message = "401: Authentication failed"
+        // };
+        // try
+        // {
+        //     context.Response.StatusCode = 401;
+        // }
+        // catch (Exception ex)
+        // {
+        //     logger.LogError(ex, $"Failed to set status code to 401 when athentication failed for {context.Principal?.Identity?.Name} to {context.Request.Path}: {ex.Message}");
+        //     // Don't fail trying to error
+        // }
+        // await context.Response.WriteAsJsonAsync(res);
     }
 }
